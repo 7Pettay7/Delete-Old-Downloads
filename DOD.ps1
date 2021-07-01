10fdfe5712396ba5279d060ef0518bdfd237f688
7Pettay7 <benpettay@gmail.com>
2021-06-30 23:01:58 -0400

    comment clarification

--- DOD.ps1
+++ DOD.ps1
@@ -2,18 +2,18 @@
 # Author: 7Pettay7
 
 # how many days old the downloads need to be to be deleted
-    # NOTE: must be a negative number to ensure substraction from todays date
+    # NOTE: must be a negative number for the script to subtract from the current date
 $daysOld = -90
 
-# optional email info, you must have two seperate emails; one for sending and the other for receiving; only need PW for sender
-    # make sure to uncomment the 'MailResults' function at the bottom of script
-$sendingEmail = ""
-$senderPw = ""
-$receivingEmail = ""
-$smtpServer = ""
-$port = 0
+# optional email inputs, see below for examples
+    # make sure to uncomment the 'MailResults' function at the bottom of script if you would like to use this feature
+$sendingEmail = "" # sender@gmail.com
+$senderPw = "" # SenderPassword123
+$receivingEmail = "" # recipient@gmail.com
+$smtpServer = "" # smtp.gmail.com
+$port = 0 # 587
 
-# grabs downloads older than the specified amount of days for later use
+# grabs downloads older than the specified amount of days
 function GetTargetDownloads() {
     $user = $env:USERNAME
     $downloads = "C:\Users\$user\Downloads"
@@ -26,13 +26,12 @@ function DeleteTargetDownloads() {
         Remove-Item -Recurse -Force
 }
 
-# function to send results to an email
+# function to put together all email info and send results
 function MailResults() {
-    # email parameters
     $credentials = New-Object Management.Automation.PSCredential $sendingEmail,
         ($senderPw | ConvertTo-SecureString -AsPlainText -Force)
     $messageParams = @{
-    Subject = "DOD Script Results"
+    Subject = "Downloads Deleted - DOD"
     Body = GetTargetDownloads |
         Sort-Object -Property LastWriteTime -Descending |
         Select-Object Name,LastWriteTime |
